1946a1609f87f39b18481d5fefd8ebd0ca790373
Shennan <thomashuet7@gmail.com>
2021-01-16 20:25:34 +0100

    up

--- c14functions.R
+++ c14functions.R
@@ -1,7 +1,16 @@
 library(oxcAAR)
 library(c14bazAAR)
 
-radonC14 <- get_c14data("radon")
+radonC14 <- get_c14data("radon") # load
+
+# retrieve main info from a date
+f.info.a.date <- function(a.db, a.dat){
+  col.nmes <- c("site", "labnr","c14age","c14std", "shortref")
+  df <- a.db[a.db$labnr == a.dat, col.nmes]
+  return(df)
+}
+
+f.info.a.date(radonC14, 'Ly-11332')
 
 ## Not run: 
 x <- oxcalCalibrate(c(5000, 4500, 3000), c(20, 50, 60))
