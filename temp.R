0da498a57f534c568f76f92a54dd148e10354b07
Shennan <thomashuet7@gmail.com>
2021-01-25 09:37:50 +0100

    bib

--- temp.R
+++ temp.R
@@ -1,18 +1,27 @@
-ui <- fluidPage(
-  sidebarPanel(
-    shiny::numericInput("bp", "BP", 5600),
-    shiny::numericInput("sd", "+/-", 60)),
-  mainPanel(
-    plotOutput("plot"))
-)
-server <- function(input, output) {
-  output$plot <- renderPlot({
-    c14date <- Bchron::BchronCalibrate(ages=input$bp,
-                               ageSds=input$sd,
-                               calCurves='intcal13',
-                               ids='date')
-    plot(c14date)
-  })
-}
-shinyApp(ui, server)
+library(openxlsx)
+library(bibtex)
+library(RefManageR)
 
+# path <- "C:/Users/supernova/Dropbox/My PC (supernova-pc)/Desktop/NeoNet/"
+path <- "/neonet/"
+fich <- "14C_DATES_v3_France_5.xlsx"
+df <- openxlsx::read.xlsx(paste0(path, fich), skipEmptyRows=TRUE)
+
+## bibref
+bibrefs <- "references_france.bib"
+bib <- read.bib(paste0(path, bibrefs))
+# path.bib <- paste0(path, "references_france.bib")
+# bib <- read.bib(path.bib)
+# print(bib, style = "Bibtex")
+keys.bib <- as.data.frame(unique(df$bib_url[grepl("^[[:upper:]]", df$bib_url)]))
+names(keys.bib)[1] <- "bib.key"
+for(i in 1:nrow(keys.bib)){
+  print(i)
+  keys.bib[i, "reference"] <- bib[c(keys.bib[i, "bib.key"])]
+}
+
+
+## dois
+dois <- df$bib_url[grepl("^http", df$bib_url)]
+a <- GetBibEntryWithDOI(dois[1])
+class(a)
