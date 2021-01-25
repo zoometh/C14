880283e4e311cd00b213c9e364a327644bbeda54
Shennan <thomashuet7@gmail.com>
2021-01-25 18:22:57 +0100

    references

--- referencebib_1.R
+++ referencebib_1.R
@@ -15,24 +15,24 @@ if(reload.all){
   df <- openxlsx::read.xlsx(paste0(path, fich), skipEmptyRows=TRUE)
   # a shorter df for uniques references
   uniq.refs <- unique(df[c("bib", "bib_url")])
-  uniq.bib <- as.data.frame(unique(df$bib))
-  names(uniq.refs)[1] <- "ref"
+  # uniq.bib <- as.data.frame(unique(df$bib))
+  names(uniq.refs) <- c("short.ref","key.or.doi")
   # # sample - - - - - - - -
   # uniq.refs <- uniq.refs[grepl("^10\\.", uniq.refs[, "ref"]), "ref"]
   # uniq.refs <- as.data.frame(uniq.refs)
   # names(uniq.refs)[1] <- "ref"
   # - - - - - - - - - - - -
-  uniq.refs$reference <- uniq.refs$bib_url # by default
+  uniq.refs$long.ref <- uniq.refs$key.or.doi # by default
   for(i in 1:nrow(uniq.refs)){
     # i <- 31
     flag <- 0
-    a.ref <- uniq.refs[i,"bib_url"]
+    a.ref <- uniq.refs[i,"key.or.doi"]
     print(a.ref)
     # bibtex - - - - - - - - - - - - - - - - - -
     if(grepl("^[[:upper:]]", a.ref)){
       a.bibref <- capture.output(print(bib[c(a.ref)]))
       a.citation <- paste0(a.bibref, collapse = " ")
-      uniq.refs[i, "reference"] <- a.citation
+      uniq.refs[i, "long.ref"] <- a.citation
       flag <- 1
     }
     # dois (all start with '10.') - - - - - - - -
@@ -41,12 +41,12 @@ if(reload.all){
       tryCatch(
         expr = {
           # uniq.refs[i, "reference"] <- GetBibEntryWithDOI(a.ref)
-          uniq.refs[i, "reference"] <- cr_cn(a.ref, format = "text") %>% 
+          uniq.refs[i, "long.ref"] <- cr_cn(a.ref, format = "text") %>% 
             map_chr(., pluck, 1)
         },
         error = function(e){
           print("err")
-          uniq.refs[i, "reference"] <- "MISSING REF"
+          uniq.refs[i, "long.ref"] <- "MISSING REF"
         },
         warning=function(cond) {
           # uniq.refs[i, "reference"] <- "a.ref" # not working
@@ -56,52 +56,12 @@ if(reload.all){
     }
     # others
     if(flag == 0){
-      uniq.refs[i, "reference"] <- "MISSING REF"
+      uniq.refs[i, "long.ref"] <- "MISSING REF"
     }
   }
   write.table(uniq.refs,"neonet/references.tsv", sep="\t", row.names=FALSE)
 }
 
-# 
-# # redo a loop on dois
-# for(i in 1:nrow(uniq.refs)){
-#   a.missing.doi <- uniq.refs[i,"reference"]
-#   if(grepl("^10\\.", a.missing.doi)){
-#     print(a.missing.doi)
-#     tryCatch(
-#       expr = {
-#         # uniq.refs[i, "reference"] <- GetBibEntryWithDOI(a.ref)
-#         uniq.refs[i, "reference"] <- cr_cn(a.missing.doi, format = "text") %>% 
-#           map_chr(., pluck, 1)
-#       },
-#       error = function(e){
-#         print("err")
-#         uniq.refs[i, "reference"] <- "a.ref"
-#       },
-#       warning=function(cond) {
-#         # uniq.refs[i, "reference"] <- "a.ref" # not working
-#         print("  - DOI not recover")
-#       })
-#     flag <- 1
-#   }
-# }
-
-# "10.1016/j.quaint.2020.10.080"
-# my_dois <- c("10.1016/j.quaint.2020.10.080")
-# cr_cn(my_dois, format = "bibtex") %>%
-#   map_chr(., pluck, 1)
-# 
-# 
-# # Use c() to create a vector of DOIs
-# my_dois <- c("10.2139/ssrn.2697412", "10.1016/j.joi.2016.08.002", "10.1371/journal.pone.0020961", 
-#              "10.3389/fpsyg.2018.01487", "10.1038/d41586-018-00104-7", "10.12688/f1000research.8460.2", 
-#              "10.7551/mitpress/9286.001.0001")
-# 
-# # Use cr_cn to get back citations formatted in Chicago for those DOIs
-# my_citations <- rcrossref::cr_cn(my_dois[2], format = "text", style = "chicago-note-bibliography") %>% 
-#   purrr::map_chr(., purrr::pluck, 1)
-# 
-# # print the formatted citations
-# my_citations
-
+references.df <- read.csv("neonet/references.tsv", sep = "\t")
+View(references.df)
 
