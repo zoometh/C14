b2f2d511218536661bdc75cbd481afeffb0c68d2
Shennan <thomashuet7@gmail.com>
2021-01-25 21:38:08 +0100

    refs

--- referencebib_1.R
+++ referencebib_1.R
@@ -6,19 +6,34 @@ library(purrr)
 library(dplyr)
 
 
-c14.to.github <- T
-reload.all.ref <- F
+c14data.to.github <- F
+c14ref.to.github <- F
+join.c14data.and.c14ref <- T
 
-path <- "C:/Users/supernova/Dropbox/My PC (supernova-pc)/Desktop/NeoNet/"
-fich <- "14C_DATES_v3_France_7.xlsx"
-df <- openxlsx::read.xlsx(paste0(path, fich), skipEmptyRows=TRUE)
+gh.master <- 'https://raw.github.com/zoometh/C14/master/' # github 'C14' folder
+gh.master <- paste0(getwd(),"/")
+path.data <- "C:/Users/supernova/Dropbox/My PC (supernova-pc)/Desktop/NeoNet/"
+fich <- "14C_DATES_v3_France_8.xlsx"
+df <- openxlsx::read.xlsx(paste0(path.data, fich), skipEmptyRows=TRUE)
 bibrefs <- paste0(getwd(),"/neonet/references_france.bib")
-if(c14.to.github){
+
+if(join.c14data.and.c14ref){
+  # load .tsv from GitHub
+  c14data.url <- paste0(gh.master, 'neonet/c14data.tsv')
+  c14data <- read.csv(c14data.url, sep = "\t")
+  c14ref.url <- paste0(gh.master, 'neonet/c14refs.tsv')
+  c14ref <- read.csv(c14ref.url, sep = "\t")
+  # merge on key
+  df.tot <- merge(c14data, c14ref, by.x="bib_url", by.y="key.or.doi", all.x = T)
+  # renames before run app ()
+  colnames(df.tot)[which(names(df.tot) == "long.ref")] <- "bib"
+}
+if(c14data.to.github){
   # create .tsv from the .xlsx file to be
   # put file on GitHub
   write.table(df,"neonet/c14data.tsv", sep="\t", row.names=FALSE)
 }
-if(reload.all.ref){
+if(c14ref.to.github){
   # for a .xlsx file of 14C
   # recalcultate the "long.ref" value from BibTex entries (file 'references_france.bib')
   # or DOIs for unique references 
@@ -70,8 +85,8 @@ if(reload.all.ref){
       uniq.refs[i, "long.ref"] <- "MISSING REF"
     }
   }
-  write.table(uniq.refs,"neonet/references.tsv", sep="\t", row.names=FALSE)
-  references.df <- read.csv("neonet/references.tsv", sep = "\t")
+  write.table(uniq.refs,"neonet/c14refs.tsv", sep="\t", row.names=FALSE)
+  references.df <- read.csv("neonet/c14ref.tsv", sep = "\t")
   View(references.df)
 }
 
