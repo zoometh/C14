0290eb71a0317ae178fc9874e35fd77d249daf31
Shennan <thomashuet7@gmail.com>
2021-02-26 23:41:08 +0100

    varia

--- func/c14functions.R
+++ func/c14functions.R
@@ -1,6 +1,7 @@
 library(oxcAAR)
 library(c14bazAAR)
 library(RPostgreSQL)
+library(DescTools)
 
 
 
@@ -9,7 +10,7 @@ dbs <- c("radon","calpal","euroevol","pacea","14cpalaeolithic") # varia
 # retrieve from selected code
 a.dat <- 'Ly-5185'
 # retrieve selected info
-get.infos.PG <- c("site", "code","bp","delta", "bib", "taxon")
+get.infos.PG <- c("site", "code","bp","delta", "us", "taxon", "bib")
 get.infos.out <- c("site", "labnr","c14age","c14std", "shortref")
 
 ## functions
@@ -28,32 +29,53 @@ read.c14.from.PG <- function(){
   return(c14s)
 }
 
-f.info.a.date <- function(a.db, a.dat){
+f.info.a.date <- function(a.db, a.dt){
+  a.dat <- unlist(strsplit(a.dt,':'))[2]
+  a.cat <- unlist(strsplit(a.dt,':'))[1]
   # retrieve main info for a selected date
-  if(a.db == 'PG'){
-    c14s <- read.c14.from.PG()
-    df <- c14s[c14s$code == a.dat, get.infos.PG]
-    df <- df[!is.na(df$code), ]
-  } else {
-    dbC14 <- get_c14data(dbs)
-    df <- dbC14[dbC14$labnr == a.dat, get.infos.out]
-    df <- df[!is.na(df$labnr), ]
-    # TODO: filters (country, etc.)
-    # sdb <- dbC14 %>%
-    #   fix_database_country_name() %>%
-    #   determine_country_by_coordinate()
-    # # clean up
-    # sdb.Fr <- sdb[sdb$country_thes == "France",]
-    # sdb.Fr <- sdb.Fr[sdb.Fr$lat <= 46,]
-    # sdb.Fr <- sdb.Fr[sdb.Fr$c14age <= 8000,]
-    # sdb.Fr <- sdb.Fr[sdb.Fr$c14age >= 5000,]
+  if(a.cat == "code"){
+    if(a.db == 'PG'){
+      c14s <- read.c14.from.PG()
+      df <- c14s[c14s$code %like any% a.dat, get.infos.PG]
+      df <- df[!is.na(df$code), ]
+    } else {
+      dbC14 <- get_c14data(dbs)
+      df <- dbC14[dbC14$labnr == a.dat, get.infos.out]
+      df <- df[!is.na(df$labnr), ]
+      # TODO: filters (country, etc.)
+      # sdb <- dbC14 %>%
+      #   fix_database_country_name() %>%
+      #   determine_country_by_coordinate()
+      # # clean up
+      # sdb.Fr <- sdb[sdb$country_thes == "France",]
+      # sdb.Fr <- sdb.Fr[sdb.Fr$lat <= 46,]
+      # sdb.Fr <- sdb.Fr[sdb.Fr$c14age <= 8000,]
+      # sdb.Fr <- sdb.Fr[sdb.Fr$c14age >= 5000,]
     }
+  }
+  if(a.cat == "site"){
+    if(a.db == 'PG'){
+      c14s <- read.c14.from.PG()
+      df <- c14s[c14s$site %like any% a.dat, get.infos.PG]
+      df <- df[!is.na(df$code), ]
+    }
+  }
+  df <- as.data.frame(df)
+  df <- df[order(df$code),]
   return(as.data.frame(df))
 }
 
 ## call
+a.dt <- 'code:LTL-8483A'
+### examples
+## code: 'code:LTL-8483A'
+# f.info.a.date("PG", 'code:LTL-8483A')
 
-f.info.a.date("PG", 'AA-53291')
+# site: "site:Pendim%"
+# f.info.a.date("PG", 'site:Pendim%')
+
+f.info.a.date("PG", 'site:%Rivoire%')
+
 
 
 
